3bf05ec645cc1a16d5f43de5664d2152b9c580af
Lusanco <lusanco@gmail.com>
2024-04-08 22:58:49 -0400

    code - number template and 5 number html

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,1 +1,48 @@
 #!/usr/bin/python3
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number_n(n):
+    if n.isdigit():
+        return f"{n} is a number"
+    else:
+        return "404 not found"
+
+
+@app.route("/number_template/<n>")
+def number_template_n(n):
+    if n.isdigit():
+        return render_template("5-number.html", number=n)
+    else:
+        return "404 not found"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
