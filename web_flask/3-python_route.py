952bd9fb5f6e839a907bd87af366eab144829805
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-09 22:21:49 -0400

    Script that starts a Flask web application and display Python followed by a text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB"
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c(text):
+    return f"C " + {text.replace('_', ' ')}
+
+@app.route('/python/<text>')
+def python(text='is cool'):
+    return f"Python " + {text.replace('_', ' ')}
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
