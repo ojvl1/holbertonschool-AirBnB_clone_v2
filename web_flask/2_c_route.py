c35dae68cbd5828358fd54f267fa6f3ced298927
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-09 22:20:48 -0400

    fix

--- web_flask/2_c_route.py
+++ web_flask/2_c_route.py
@@ -17,7 +17,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    return f"C {text.replace('_', ' ')}"
+    return f"C " + {text.replace('_', ' ')}
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
