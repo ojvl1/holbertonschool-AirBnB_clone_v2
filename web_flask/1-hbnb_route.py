f001cdfb4483a38fdfe7c7af9ebceb4270f710dd
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-10 11:08:45 -0400

    pycodestyle- add blank lines

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,17 +3,21 @@
 Script that starts a Flask web application
 """
 
+
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hello():
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
