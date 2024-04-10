7c79049e11f48d3cd8f7cf3cb44be5b3085f7eb4
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-10 14:40:16 -0400

    add render template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,7 +4,7 @@ Script that starts a Flask web application
 """
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -37,7 +37,7 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return f"Number: {n}"
+    return render_template('5-number.html')
 
 
 if __name__ == '__main__':
