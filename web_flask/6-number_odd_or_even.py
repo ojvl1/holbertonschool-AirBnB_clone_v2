072855ab550536fc3d00ab279fd6b40210128cdd
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-10 18:10:30 -0400

    fix the module

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -41,7 +41,7 @@ def number_template(n):
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def odd_or_even(n, even, odd):
+def odd_or_even(n):
     return render_template('6-number_odd_or_even.html', number=n)
 
 
