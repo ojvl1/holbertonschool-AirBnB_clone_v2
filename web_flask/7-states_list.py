43a5ad7aab8a476b826ebce289b16cb34a3c2dc0
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-12 14:46:49 -0400

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,60 @@
 #!/usr/bin/python3
 """Script that starts a Flask web application"""
 
+import sys
 from flask import Flask, render_template
+sys.path.append('/home/osvaldo_jose1/holbertonschool-AirBnB_clone_v2')
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template("5-number.html", number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
 @app.route('/states_list', strict_slashes=False)
 def display_states():
     """Render state_list.html"""
-    state = storage.all()
-    return render_template('7-states_list.html', state=state)
+    states = storage.all()
+    states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext('/states_list')
+@app.teardown_appcontext
 def teardown(exception):
     """Remove SQLAlchemy Session"""
     storage.close()
